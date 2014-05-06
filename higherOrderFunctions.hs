62ab431f1f0f4560995c89ebcdf5093752062a09
Lucas Viola <lucasmatzenbacher@gmail.com>
2014-05-05 21:42:08 -0300

    stopped on filter

--- higherOrderFunctions.hs
+++ higherOrderFunctions.hs
@@ -9,6 +9,34 @@ zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+	where g x y = f y x
+
+-- Usage: map (map (^2)) [[1,2],[3,4,5],[6,7,8]]
 map' :: (a -> b) -> [a] -> [b]
 map' _ [] = []
 map' f (x:xs) = f x : map' f xs
+
+
+-- Usage: filter (`elem` ['A'..'Z']) "the quick brown fox jumpS over the lAzy caT and nothing hAppeNs."
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+	| p x = x : filter' p xs
+	| otherwise = filter' p xs
+
+-- Quicksort using filters
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort (filter (<=x) xs)  
+        biggerSorted = quicksort (filter (>x) xs)   
+    in  smallerSorted ++ [x] ++ biggerSorted
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [1000000,99999..])
+	where p x = x `mod` 3234 == 0
+
+numLongChains :: Int
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100])
