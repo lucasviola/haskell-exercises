0ea00fb06ce730d67f319c0a0e01fa4271055676
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-12 16:23:23 -0300

    criada função findTelefone usando maybe monad para não gerar exceção

--- phoneBook.hs
+++ phoneBook.hs
@@ -1,4 +1,5 @@
 import qualified Data.Map as Map
+import Data.Maybe
 
 -- Isso é como se fosse um HashMap.
 -- É simplesmente uma lista de tuplas
@@ -9,5 +10,13 @@ phoneBook =
 	,("Mateus", "99002345")
 	]
 
+-- Unsafe: Quando não encontra nada gera exceção
 findTelefone :: (Eq k) => k -> [(k,v)] -> v
 findTelefone key xs = snd . head . filter (\(k,v) -> key == k) $ xs
+
+-- Safe: Usando Maybe Monad
+findTelefone' :: (Eq k) => k -> [(k,v)] -> Maybe v  
+findTelefone' key [] = Nothing  
+findTelefone' key ((k,v):xs) = if key == k  
+                            then Just v  
+                            else findTelefone' key xs 
