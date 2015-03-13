6c20f8d7427a02ccd72db04364055acb3ebbfcf6
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-13 01:56:01 -0300

    Transformei Shapes em um Modulo

---
+++ Shapes.hs
@@ -0,0 +1,18 @@
+module Shapes 
+( Point(..)
+, Shape(..)
+, surface
+) where
+
+-- Value Constructor (Criando um tipo, basicamente)
+
+-- Duas dimensões
+data Point = Point Float Float deriving (Show)
+
+-- Três dimensões
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+
+
+surface :: Shape -> Float
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) =  (abs $ x2 - x1) * (abs $ y2 - y1)
