169961356c64c5204519ea886dd1eaf4a803353c
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-13 02:12:54 -0300

    comentários adicionais

--- Shapes.hs
+++ Shapes.hs
@@ -1,6 +1,6 @@
 module Shapes 
-( Point(..)
-, Shape(..)
+( Point
+, Shape(..) 
 , surface
 ) where
 
@@ -12,7 +12,8 @@ data Point = Point Float Float deriving (Show)
 -- Três dimensões
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
-
+-- Ex: surface (Circle (Point 1 2) 3)
+-- Ex: surface (Rectangle (Point 4 5) (Point 6 7))
 surface :: Shape -> Float
 surface (Circle _ r) = pi * r ^ 2
 surface (Rectangle (Point x1 y1) (Point x2 y2)) =  (abs $ x2 - x1) * (abs $ y2 - y1)
