f1a6569a1d5bbd45d7c1fa0be13794e1bb6af0c9
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-14 19:50:54 -0300

    finalmente cheguei em input e output. All heil side-effects and impurity

---
+++ io/input-output.hs
@@ -0,0 +1,5 @@
+main = do
+	putStrLn "Olá, digite o seu nome:"
+	nome <- getLine
+	putStrLn ("Ola " ++ nome ++ ", voce chegou ate aqui. Voce eh demais.")
+	putStrLn ("Bem-vindo ao motherfucking capitulo 9!")
