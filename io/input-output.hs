589cdd5310323b19ccabe24cdf6a88dcdb35f83e
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-15 01:38:05 -0300

    a magia do return

--- io/input-output.hs
+++ io/input-output.hs
@@ -1,10 +1,4 @@
-import Data.Char
-
 main = do
-	putStrLn "Qual o seu primeiro nome?"
-	primeiroNome <- getLine
-	putStrLn "Qual o seu segundo nome?"
-	segundoNome <- getLine
-    let primeiroNomeMaiusculo = map toUpper primeiroNome  
-        segundoNomeMaiusculo = map toUpper segundoNome 
-	putStrLn $ "Ola " ++ primeiroNomeMaiusculo ++ " " ++ segundoNomeMaiusculo ++ ", como tu andas?"
+	a <- return "hell"
+	b <- return "yeah"
+	putStrLn $ a ++ " " ++ b
