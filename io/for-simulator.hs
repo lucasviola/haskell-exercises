2b7ab88a3a1c18bc884098a1eb953cc13b19e963
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-15 02:14:31 -0300

    comentarios adicionais

--- io/for-simulator.hs
+++ io/for-simulator.hs
@@ -7,3 +7,7 @@ main = do
 		return cor)
 	putStrLn "As cores que voce associa com 1, 2, 3, 4 sao: "
 	mapM putStrLn cores
+
+-- o forM executa o lambda que tá dentro do DO BLOCK n vezes e dá um bind do
+	-- return em cores
+-- O  mapM mapeia os numeros com as cores
