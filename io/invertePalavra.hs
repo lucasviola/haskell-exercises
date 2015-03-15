e63b1611a57641223312e07c90181d4362a0a1ed
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-15 01:37:55 -0300

    comentários adicionais

--- io/invertePalavra.hs
+++ io/invertePalavra.hs
@@ -2,10 +2,15 @@ main = do
 	putStrLn "Digite uma palavra ou frase para ser invertida:"
 	linha <- getLine
 	if null linha
-		then return ()
+		then return () -- Atenção: Isso é uma Monad! Não termina a execução do programa. Transforma a linha em IO.
 		else do
 			putStrLn $ invertePalavra linha
 			main
 
 invertePalavra :: String -> String
 invertePalavra = unwords . map reverse . words
+
+-- Explicação da função composta:
+-- unwords (map reverse (words linha))
+-- words: Transforma uma string em uma lista de palavras
+-- unwords: Transforma uma lista de palavras em uma String
